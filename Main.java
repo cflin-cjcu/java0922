d95888d0a8adcc959db749cadbb3e6f2e83bd996
cflin <cflin@mail.cjcu.edu.tw>
2022-09-22 15:31:04 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Human a = new Human("cflin",177.0,48.0);
+        Human b = new Human();
+        System.out.println("YAYA!!"+a.getName());
+        a.setName("Bill");
+        System.out.println("YAYA!!"+a.getName());
+        a.hello();
+        b.hello();
+        System.out.printf("%.2f\n",a.calcBMI()); 
+        
+        System.out.println(a.myHeath());
+
+    }
+}
---
+++ MyMessage.java
@@ -0,0 +1,15 @@
+public class MyMessage {
+
+    // public MyMessage() {
+    //     System.out.println("Guest, HELLO!!!");
+    // }
+
+    public  MyMessage(String string) {
+        System.out.println(string + ", HELLO!!!");
+    }
+
+    public MyMessage() {
+    }
+
+
+}
